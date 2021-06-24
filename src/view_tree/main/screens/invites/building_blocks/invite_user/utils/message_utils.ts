119facb8fdc7cf672ee8a15d0dfa4d7cc7aa2402
Danny Geisz <dannygeisz@berkeley.edu>
2021-06-24 09:12:35 -0600

    Altered invite message

--- src/view_tree/main/screens/invites/building_blocks/invite_user/utils/message_utils.ts
+++ src/view_tree/main/screens/invites/building_blocks/invite_user/utils/message_utils.ts
@@ -1,3 +1,3 @@
 export function createMessage(firstName: string, inviteCode: string) {
-    return `Hey ${firstName}! I have an invite to Digitari and want you to join! \n\nYou can get Digitari on the App Store or Play Store, and use the invite code ${inviteCode} to unlock your account!`;
+    return `Hey ${firstName}! I have an invite to Digitari and want you to join! Here's the link. https://playdigitari.com/app\n\nOnce you have the app, use the invite code ${inviteCode} to unlock your account!`;
 }
